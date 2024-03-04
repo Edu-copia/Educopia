0dc89a7af8c7251654b15d67699ea020edc8312e
jonbingkim <jonkimtw@gmail.com>
2024-03-03 20:02:35 -0800

    DB connected

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello, Educopia! Test!</h1>
-      <p>Another Test</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello, Educopia! Test!</h1>
+			<p>Another Test</p>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,12 +1,25 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, { Express, Request, Response, NextFunction } from "express";
+import supabase from "../db";
 const router = express.Router();
 
-router.get('', (req: Request, res: Response) => {
+//set up a quick function to check if the database is connected -JK
+async function fetchUserData() {
+	try {
+		const { data: users, error } = await supabase.from("users").select("*");
+		if (error) {
+			throw new Error("Error fetching users: " + error.message);
+		}
+		console.log(users);
+		return users;
+	} catch (error) {
+		console.error("An error occurred while fetching users:", error);
+		return null;
+	}
+}
+router.get("/", (req: Request, res: Response) => {
+	res.json(fetchUserData());
+});
 
-})
-
-router.post('', (req: Request, res: Response) => {
-
-})
+router.post("", (req: Request, res: Response) => {});
 
 export default router;
