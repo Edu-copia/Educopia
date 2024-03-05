66fd2f241067f68a332648fa3a33ddf85be5925a
jonbingkim <jonkimtw@gmail.com>
2024-03-04 19:45:34 -0800

    routes/queries

---
+++ src/client/wishlist/wishlist.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Wishlist = () => {
+  return (
+    <>
+    </>
+  )
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,9 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser'
-import apiRouter from './routes/api'
-import 'dotenv/config'
+import express, { Express, Request, Response, NextFunction } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import apiRouter from "./routes/api";
+import wishlistRouter from "./routes/wishlist";
+import "dotenv/config";
 
 const app: Express = express();
 // enable parsing of URL-encoded form data
@@ -12,33 +13,36 @@ app.use(express.json());
 //handle cookies
 app.use(cookieParser());
 //npm start after build - serves index.html:
-if (process.env.NODE_ENV === 'production') {
-    //statically serve everything in the dist folder on the route '/dist'
-    app.use('/dist', express.static(path.join(__dirname, '../dist/')));
-    // serve index.html on the route '/'
-    app.get('/', (req: Request, res: Response) => {
-        return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+	//statically serve everything in the dist folder on the route '/dist'
+	app.use("/dist", express.static(path.join(__dirname, "../dist/")));
+	// serve index.html on the route '/'
+	app.get("/", (req: Request, res: Response) => {
+		return res.status(200).sendFile(path.join(__dirname, "../index.html"));
+	});
 }
 
 //Routes
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
+app.use("/api/wishlist", wishlistRouter);
 //Page Not Found
-app.use('*', (req: Request, res: Response) => {
-    res.status(404).send('Not Found');
-})
+app.use("*", (req: Request, res: Response) => {
+	res.status(404).send("Not Found");
+});
 
 //Global Error Handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    const defaultErr = {
-        log: 'unknown error handler caught in middleware',
-        status: 400,
-        message: { err: 'An error occured' },
-    }
-    const errorObj = Object.assign({}, defaultErr, err);
-    return res.status(errorObj.status).send(errorObj.message);
-})
+	const defaultErr = {
+		log: "unknown error handler caught in middleware",
+		status: 400,
+		message: { err: "An error occured" },
+	};
+	const errorObj = Object.assign({}, defaultErr, err);
+	return res.status(errorObj.status).send(errorObj.message);
+});
 //Listening on port 3000
-app.listen(3000, () => { console.log('Listening on port 3000...')});
+app.listen(3000, () => {
+	console.log("Listening on port 3000...");
+});
 //export app
 export default app;
