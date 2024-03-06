5e1c01147833e5a045b187f414bfa65bf91263d5
jonbingkim <jonkimtw@gmail.com>
2024-03-05 18:06:32 -0800

    minor updates for syntax

--- src/server/controllers/wishlistController.ts
+++ src/server/controllers/wishlistController.ts
@@ -5,19 +5,19 @@ export const wishlistController = {
 	getItems: async (req: Request, res: Response, next: NextFunction) => {
 		try {
 			const items = await sql`
-                SELECT 
-								items.item_id,
-								items.item_name,
-								items.current_quantity,
-								items.monthly_quantity_usage,
-								items.item_link,
-								items.teacher_comment,
-								wishlist.quantity_needed,
-								wishlist.wishlist_id
-                FROM
-                    wishlist 
-                INNER JOIN
-                    items ON wishlist.item_id = items.item_id`;
+			SELECT 
+			items.item_id,
+			items.item_name,
+			items.current_quantity,
+			items.monthly_quantity_usage,
+			items.item_link,
+			items.teacher_comment,
+			wishlist.quantity_needed,
+			wishlist.wishlist_id
+			FROM
+				wishlist 
+			INNER JOIN
+				items ON wishlist.item_id = items.item_id`;
 			res.locals.items = items;
 			console.log(items);
 
