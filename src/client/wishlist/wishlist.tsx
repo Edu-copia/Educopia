ec2f9d3b833c7c993a3fcc0d77f1fdc5e7773bc4
jonbingkim <jonkimtw@gmail.com>
2024-03-05 16:51:01 -0800

    merging db setup and wishlist

--- src/client/wishlist/wishlist.tsx
+++ src/client/wishlist/wishlist.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Wishlist = () => {
-  return (
-    <>
-    </>
-  )
-}
+	useEffect(() => {}, []);
+	return <></>;
+};
