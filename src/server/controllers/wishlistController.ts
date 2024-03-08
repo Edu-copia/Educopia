45b4fc2e49c6a1811bd37bd58f216d1960400ed8
jonbingkim <jonkimtw@gmail.com>
2024-03-07 19:32:33 -0800

    fixed routes and queries

--- src/server/controllers/wishlistController.ts
+++ src/server/controllers/wishlistController.ts
@@ -6,18 +6,14 @@ export const wishlistController = {
 		try {
 			const items = await sql`
 			SELECT 
-			items.item_id,
-			items.item_name,
-			items.current_quantity,
-			items.monthly_quantity_usage,
-			items.item_link,
-			items.teacher_comment,
-			wishlist.quantity_needed,
-			wishlist.wishlist_id
+			item_id,
+			item_name,
+			item_link,
+			teacher_comment
 			FROM
-				wishlist 
-			INNER JOIN
-				items ON wishlist.item_id = items.item_id`;
+			items
+			WHERE
+			current_quantity/monthly_quantity_usage <= 2`;
 			res.locals.items = items;
 			console.log(items);
 
@@ -32,23 +28,18 @@ export const wishlistController = {
 			console.log("this is the id", id);
 			const item = await sql`
 			SELECT 
-			items.item_id,
-			items.item_name,
-			items.current_quantity,
-			items.monthly_quantity_usage,
-			items.item_link,
-			items.teacher_comment,
-			wishlist.quantity_needed,
-			wishlist.wishlist_id
+			item_name,
+			item_link,
+			teacher_comment
 			FROM
-				wishlist 
-			INNER JOIN
-				items ON wishlist.item_id = items.${id}`;
+			items
+			WHERE
+			item_id = ${id}`;
 			// console.log(item);
 			res.locals.item = item;
 			next();
 		} catch (err) {
-			console.log;
+			next(err);
 		}
 	},
 	itemfulfillment: async (req: Request, res: Response, next: NextFunction) => {
@@ -71,15 +62,10 @@ export const wishlistController = {
 			console.log("added into fulfillments table");
 
 			await sql`
-			UPDATE wishlist
-			SET quantity_needed = quantity_needed - ${quantity_purchased}
+			UPDATE items
+			SET current_quantity = current_quantity + ${quantity_purchased}
 			WHERE item_id = ${item_id}`;
-			console.log("updated wishlist");
-
-			await sql`
-			DELETE FROM wishlist
-			WHERE item_id = ${item_id} AND quantity_needed <= 0`;
-			console.log("Deleted item from wishlist");
+			console.log("inventory updated");
 			next();
 		} catch (err) {
 			next(err);
