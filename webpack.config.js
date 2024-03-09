24f52ff0af44828fe8a4743b852cd0cc34dfc49d
Derek Coughlan <132537508+derekcoughlan@users.noreply.github.com>
2024-03-08 20:39:41 -0800

    removed shadcn

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,8 @@ const config = {
     filename: "bundle.js",
   },
   devServer: {
-    static: {
-      publicPath: '/',
-      directory: path.resolve(__dirname, '/dist')
-    },
     open: true,
+    host: "localhost",
     proxy: [
       {
         context: ["/api"],
@@ -61,7 +58,6 @@ const config = {
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
-  devtool: 'source-map'
 };
 
 module.exports = () => {
